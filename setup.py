c0a4cea7222b17977b47ce4fa39f6a0729939f58
lotegau <gaurav.lotekar@msci.com>
2022-01-18 14:40:31 +0530

    added few changes

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='adfmapping',
     version='0.1.0',
     author='Gaurav Lotekar',
+    author_email='gauravnlotekar@gmail.com',
+    url='https://github.com/gogi2811/adfmapping',
+    keywords='Azure ADF mapping ADF mapping JSON json',
     py_modules=['adfmapping'],
     install_requires=[
         'Click',
